c42a304eb5454bfb6398cd46901757d206861d54
Jose Angel Colin Najera <josecolin99@gmail.com>
2023-09-22 00:10:31 -0600

    feat(tools.youtube): Added Module Youtube whith function save_playlist

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-# This code was based on: https://www.tiktok.com/@nextkoolhacks/video/7279420996585573637
-
-# Python librarys
+#
+# Tools imports
+from tools import youtube
 import os
 
-# pytube library imports
-from pytube import Playlist, YouTube
-from pytube import exceptions
-
-#? BASE_DIR represents the absolute path to the parent directory of the current script.
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 folder = 'ignore_this/ELIMINAR'
-download_folder = os.path.join(BASE_DIR, folder)
-playlist = Playlist('https://www.youtube.com/playlist?list=PL_adZhO9fB0sO-tgDxLtC29wQ-T_Xs5Y2')
-count = 1
-for video in playlist:
-    print('Starting download of:')
-    print(YouTube(video).title)
-    try:
-        YouTube(video).streams.get_audio_only().download(download_folder)
-    except exceptions.AgeRestrictedError:
-        print('Video con restriccion de edad')
-    except Exception as ex:
-        print(ex)
-    print('FINISHED'.center(50, '='))
-    count += 1
-
-print()
-print('TOTAL DE DESCARGAS')
-print(count)
-
-# CodedByColin
+url = 'https://www.youtube.com/playlist?list=PL_adZhO9fB0sO-tgDxLtC29wQ-T_Xs5Y2'
+youtube.save_playlist(folder, url)
---
+++ tools/youtube.py
@@ -0,0 +1,35 @@
+# This code was based on: https://www.tiktok.com/@nextkoolhacks/video/7279420996585573637
+
+# Python librarys
+import os
+
+# pytube library imports
+from pytube import Playlist, YouTube
+from pytube import exceptions
+
+# Import Constants
+from constants import generals as const_general
+#? BASE_DIR represents the absolute path to the parent directory of the current script.
+
+def save_playlist(folder: str, url: str):
+    #!Agregar un verificador regex del folder destino
+    download_folder = os.path.join(const_general.BASE_DIR, folder)
+    playlist = Playlist(url)
+    count = 1
+    for video in playlist:
+        print('Starting download of:')
+        print(YouTube(video).title)
+        try:
+            YouTube(video).streams.get_audio_only().download(download_folder)
+        except exceptions.AgeRestrictedError:
+            print('Video con restriccion de edad')
+        except Exception as ex:
+            print(ex)
+        print('FINISHED'.center(50, '='))
+        count += 1
+
+    print()
+    print('TOTAL DE DESCARGAS')
+    print(count)
+
+# CodedByColin
