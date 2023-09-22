286b88722a6df6db4edc5a79db5317daeb1d5169
Jose Angel Colin Najera <josecolin99@gmail.com>
2023-09-21 23:59:03 -0600

    feat(Exceptios & Base): Added Exception Case & Base Dir

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-#? Original code obtained from with:: https://www.tiktok.com/@nextkoolhacks/video/7279420996585573637
+# This code was based on: https://www.tiktok.com/@nextkoolhacks/video/7279420996585573637
+
+# Python librarys
+import os
+
+# pytube library imports
 from pytube import Playlist, YouTube
 from pytube import exceptions
 
-playlist = Playlist('https://www.youtube.com/playlist?list=PLPyj1P8a2S6WUEpf-SQIFMH1MUhEXVJ81')
+#? BASE_DIR represents the absolute path to the parent directory of the current script.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+folder = 'ignore_this/ELIMINAR'
+download_folder = os.path.join(BASE_DIR, folder)
+playlist = Playlist('https://www.youtube.com/playlist?list=PL_adZhO9fB0sO-tgDxLtC29wQ-T_Xs5Y2')
+count = 1
 for video in playlist:
     print('Starting download of:')
     print(YouTube(video).title)
     try:
-        YouTube(video).streams.get_audio_only().download('C:/secret_files/Random/jimmy')
+        YouTube(video).streams.get_audio_only().download(download_folder)
     except exceptions.AgeRestrictedError:
         print('Video con restriccion de edad')
     except Exception as ex:
         print(ex)
     print('FINISHED'.center(50, '='))
+    count += 1
+
+print()
+print('TOTAL DE DESCARGAS')
+print(count)
+
+# CodedByColin
